ba900369832fac2fe1140ae4a2da1106680496da
MatthewZGong <matthewzgong@gmail.com>
2024-04-27 15:01:04 -0400

    alerts framework

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import os.path
 import threading
 import pyarrow as pa
+from dotenv import load_dotenv
 
 import csp
 from csp import ts
@@ -96,6 +97,67 @@ def push_data_to_perspective_table(data: ts[pl.DataFrame], table: PerspectiveTab
         # writer.close()
         # table.update(stream.getvalue().to_pybytes())
         table.update(data.to_pandas().to_dict(orient="records"))
+
+
+@csp.node()
+def mta_alerts(table: PerspectiveTable, timedelta: timedelta = timedelta(seconds=500)):
+    with csp.alarms():
+        a_poll = csp.alarm(bool)
+
+    with csp.start():
+        csp.schedule_alarm(a_poll, timedelta, True)
+        ds_loader = DatasetLoader()
+    
+    if csp.ticked(a_poll):
+        # csp.print("HEY I TICKED")
+        json = ds_loader.get_mta_alarms()
+
+        all_text = ""
+        for alert in json:
+            all_text += alert.alert.header_text.translation[0].text
+
+        client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        # q =  "Southbound BxM1 stops on Lexington Ave at E 96th St and E 86th St will be closed"
+        response = client.chat.completions.create(
+        messages=[{
+                "role": "system",
+                "content": "Give just all addresses as a comma separated list based off of the user message.  Give just the location if additional information is missing do not add. Make sure to return a comma  separated list of addresses. Make sure to return the address in the same language as the user message."},
+            {"role": "user", "content": all_text}],
+        model="gpt-3.5-turbo",
+        )
+
+        # print(q)
+        expl = response.choices[0].message.content
+        addresses = expl.split(",")
+        aspiringdf=[]
+        # print(expl)
+        # print(addresses)
+        geolocator = Nominatim(user_agent="DEF")
+        for address in addresses:
+            location = None
+            try:
+                location = geolocator.geocode(address+", NY")
+            except Exception as e:
+                location = None
+                sleep(.1)
+
+            if location is not None:
+                print(location.latitude, location.longitude)
+                aspiringdf.append((location.latitude, location.longitude, address))
+            else: 
+                print("No location found")
+
+        
+
+        df = pl.DataFrame(aspiringdf, schema=["lat", "lon", "address"])
+        print(df.head())
+        table.update(df.to_pandas().to_dict(orient="records"))
+    
+
+        
+    # if csp.ticked(table):
+    #     alerts = table.get_mta_alarms()
+    #     print(alerts)
         
 @csp.node
 def process_data(df: ts[pl.DataFrame]) -> ts[pl.DataFrame]:
@@ -108,6 +170,7 @@ def process_data(df: ts[pl.DataFrame]) -> ts[pl.DataFrame]:
 
             modified_df = df.clone()
 
+
             # This loop iterates over each station in the graph. For each station, it updates the 'capacity' column in the DataFrame 'modified_df'.
             # It sets the 'capacity' to 'num_bikes' where the 'station_id' matches, otherwise it retains the original 'capacity'.
             for station_id, (num_bikes, _, _, _) in graph.nodes.items():
@@ -115,12 +178,16 @@ def process_data(df: ts[pl.DataFrame]) -> ts[pl.DataFrame]:
             return modified_df
 
 @csp.graph        
-def main_graph(table: PerspectiveTable, mod_table: PerspectiveTable, interval: timedelta = timedelta(seconds=10)):
+def main_graph(table: PerspectiveTable, mod_table: PerspectiveTable, alert_table: PerspectiveTable, interval: timedelta = timedelta(seconds=10)):
     data = poll_data(interval)
     modified_df = process_data(data)
+    # mta_alerts = mta_alerts(alert_table)
+
     # data.vstack(modified_df)
     push_data_to_perspective_table(data, table)
     push_data_to_perspective_table(modified_df, mod_table)
+    # mta_alerts = mta_alerts(alert_table)
+
     # csp.print("data", data)
     # csp.print("graph", modified_df)
 
@@ -172,17 +239,27 @@ def run_app(manager: PerspectiveManager):
         index="station_id",
     )
 
+    table3 = PerspectiveTable(
+        {
+            "lat": float,
+            "lon": float,
+            "address": str,
+        },
+        index="address",
+    )
+
     # host these tables
     manager.host_table("data", table)
     manager.host_table("mod_data", table2)
+    manager.host_table("alerts_table", table3)
 
     
-    return csp.run_on_thread(main_graph, table, table2, timedelta(seconds=60), realtime=True)
+    return csp.run_on_thread(main_graph, table, table2, table3,timedelta(seconds=60), realtime=True)
 
 
 def main():
     # csp.run(main_graph, None, timedelta(seconds=60), realtime=True)
-
+    load_dotenv()
     perspective_manager = PerspectiveManager()
 
     app = make_perspective_app(perspective_manager)
