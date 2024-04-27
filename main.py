46dc65d85f030722c62cb07b1b2ea7f7401bf19b
Tri Dao <daominhtri0503@gmail.com>
2024-04-26 23:46:32 -0400

    pipeline with polars

---
+++ main.py
@@ -0,0 +1,33 @@
+import csp
+
+from csp import ts
+from datetime import timedelta
+from pipeline.pipeline import DatasetLoader
+import polars as pl
+
+@csp.node
+def poll_data(interval: timedelta) -> ts[pl.DataFrame]:
+    with csp.alarms():
+        a_poll = csp.alarm(bool)
+
+    with csp.start():
+        csp.schedule_alarm(a_poll, timedelta(), True)
+        ds_loader = DatasetLoader()
+
+    if csp.ticked(a_poll):
+        status = ds_loader.get_station_status()
+
+        csp.schedule_alarm(a_poll, interval, True)
+        return status
+
+@csp.graph
+def main_graph(interval: timedelta = timedelta(seconds=60)):
+    data = poll_data(interval)
+    csp.print("data", data)
+
+
+def main():
+    csp.run(main_graph, timedelta(seconds=60), realtime=True)
+
+if __name__ == '__main__':
+    main()
